080849e16a69965e010d645eff0ef1eb45d3dcad
nyomansunima <nyomansunima@gmail.com>
2023-05-25 09:42:51 +0800

    🍀 update: adding the config and css

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,10 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/devtools'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/devtools',
+    'nuxt-appwrite',
+    '@nuxt/image-edge'
+  ],
+  css: ['~/assets/styles/globals.css'],
+  imports: {
+    dirs: ['composables/**']
+  },
+  components: {
+    dirs: [
+      {
+        path: '~/components',
+        pathPrefix: false
+      }
+    ]
+  },
   nitro: {
     preset: 'vercel-edge'
   },
   devtools: {
     enabled: true
+  },
+  appwrite: {
+    project: process.env.APPWRITE_PROJECT
+  },
+  runtimeConfig: {
+    public: {
+      host: ''
+    }
   }
 })
