c7912d2c2680e04c6994ec83ff51f6269d03045d
nyomansunima <nyomansunima@gmail.com>
2023-05-25 09:42:10 +0800

    🔥 update: adding themes

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,5 +3,26 @@ import forms from '@tailwindcss/forms'
 import aspectRatio from '@tailwindcss/aspect-ratio'
 
 export default <Partial<Config>>{
+  theme: {
+    fontFamily: {
+      heading: ['Poppins', 'sans-serif'],
+      body: ['DM Sans', 'sans-serif']
+    },
+    screens: {
+      tablet: '640px',
+      laptop: '1024px',
+      desktop: '1280px'
+    },
+    extend: {
+      colors: {
+        lavender: '#A394FF',
+        peach: '#FFB292',
+        gum: '#FF87AB'
+      },
+      borderRadius: {
+        '4xl': '32px'
+      }
+    }
+  },
   plugins: [forms, aspectRatio]
 }
