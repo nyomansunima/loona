a667d4d3d35a1c336e1a20248458a0285c322094
nyomansunima <nyomansunima@gmail.com>
2023-06-02 11:05:42 +0800

    🎉 build: create a new layout for new event

---
+++ apps/web/layouts/new-event.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- new event header -->
+    <NewEventHeader />
+
+    <!-- all contents -->
+    <main class="container mx-auto py-32">
+      <div class="w-6/12 mx-auto">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
