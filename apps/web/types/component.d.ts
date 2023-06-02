de5be9aea0df5982c67c395fdb3ba5fa37da54ca
nyomansunima <nyomansunima@gmail.com>
2023-06-02 11:05:15 +0800

    🎉 build: create the component like button, input base props

--- apps/web/types/component.d.ts
+++ apps/web/types/component.d.ts
@@ -3,8 +3,18 @@ export interface BaseButtonProps {
   type?: 'button' | 'submit'
   disabled?: boolean
   link?: string
+  size?: 'small' | 'medium' | 'large'
 }
 
 export interface BaseButtonEmits {
   (e: 'click'): void
+}
+
+export interface BaseInputProps {
+  label?: string
+  preIcon?: string
+  sufIcon?: string
+  name: string
+  modelValue?: any
+  placeholder?: string
 }
